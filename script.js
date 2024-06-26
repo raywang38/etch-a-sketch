d8f59c21c8cdf7170379b59a0050354488d2f41c
Ray <170437057+raywang38@users.noreply.github.com>
2024-06-26 15:46:36 +1000

    add code logic to resize grid upon user input
    Input prompt allows for user to determine their
    ideal grid size and the grid adjusts to accommodate
    this.

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 const container = document.querySelector(".container");
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.width = "520px";
+container.style.width = 16 * 32 + "px";
+
+const buttonField = document.querySelector(".buttons");
 
 const selectGrid = document.createElement("button");
-selectGrid.textContent = "Grid";
+selectGrid.textContent = "Grid Configuration";
+buttonField.appendChild(selectGrid);
 
 const clearButton = document.createElement("button");
-clearButton.textContent = "Clear";
+clearButton.textContent = "Clear Grid";
+buttonField.appendChild(clearButton);
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < 16 * 16; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.style.backgroundColor = "lightGray";
+    squareDiv.style.minWidth = "30px";
+    squareDiv.style.minHeight = "30px";
+    squareDiv.style.borderColor = "black";
+    squareDiv.style.borderWidth = "1px";
+    squareDiv.style.borderStyle = "solid";
+    squareDiv.setAttribute("class", "originalSquare");
+    container.appendChild(squareDiv);
+
+    squareDiv.addEventListener("mouseover", () => {
+        squareDiv.style.backgroundColor = "black";
+    })
+
+    clearButton.addEventListener("click", () => {
+        squareDiv.style.backgroundColor = "lightGray";
+    })
+}
+
+selectGrid.addEventListener("click", () => {
+
+    let gridNumber;
+    do {
+        gridNumber = prompt("Please enter the number of squares for the grid", "");
+        gridNumber = parseInt(gridNumber);
+    } while (gridNumber < 0 || gridNumber > 100 /*|| typeof(gridNumber) == "number"*/)
+
+    const removedSquares = document.querySelectorAll(".originalSquare, .modifiedSquare");
+
+    for (const removedSquare of removedSquares) {
+        removedSquare.remove();
+    }
+
+    container.style.width = gridNumber * 32 + "px";
+
+    for (let j = 0; j < gridNumber * gridNumber; j++) {
         const squareDiv = document.createElement("div");
         squareDiv.style.backgroundColor = "lightGray";
         squareDiv.style.minWidth = "30px";
@@ -18,10 +55,16 @@ for (let i = 0; i < 16; i++) {
         squareDiv.style.borderColor = "black";
         squareDiv.style.borderWidth = "1px";
         squareDiv.style.borderStyle = "solid";
+        squareDiv.setAttribute("class", "modifiedSquare");
         container.appendChild(squareDiv);
-
+        
         squareDiv.addEventListener("mouseover", () => {
             squareDiv.style.backgroundColor = "black";
         })
+    
+        clearButton.addEventListener("click", () => {
+            squareDiv.style.backgroundColor = "lightGray";
+        })    
     }
-}
+})
+
